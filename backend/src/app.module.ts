d34a301442955966d337d9ef113e91d0c5dc76d3
Hamza Sehouli <sehoulihamza@yahoo.com>
2026-09-23 03:13:54 +0000

    Registered usercontroller, service and repository into app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './users/users.service';
+import { UserController } from './users/users.controller';
+import { UserRepository } from './users/users.repository';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService, UserRepository, PrismaService],
 })
 export class AppModule {}
