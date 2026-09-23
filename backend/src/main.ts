6e7f72fdda9f43caddb37e8fd30a411eb4bad313
Hamza Sehouli <sehoulihamza@yahoo.com>
2026-09-23 03:24:51 +0000

    Config transform interceptor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { TranformInterceptor } from './utils/tranform.interceptor';
+import { TransformInterceptor } from './utils/tranform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new TranformInterceptor());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- backend/src/utils/tranform.interceptor.ts
+++ backend/src/utils/tranform.interceptor.ts
@@ -1,9 +1,39 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  data: T;
+  message: string;
+  timestamp: string;
+  statusCode: number;
+}
 
 @Injectable()
-export class TranformInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+export class TransformInterceptor<T> implements NestInterceptor<
+  T,
+  Response<T>
+> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    const now = new Date().toISOString();
+    const statusCode = context.switchToHttp().getResponse<any>().statusCode;
+    return next
+      .handle()
+      .pipe(
+        map((data: T) => ({
+          statusCode,
+          message: 'Success',
+          timestamp: now,
+          data,
+        })),
+      );
   }
 }
